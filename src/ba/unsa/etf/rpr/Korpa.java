59dc83fec07d9cc8b1307232d3c4e2d4860de4dd
saksamovic1 <saksamovic1@etf.unsa.ba>
2021-10-21 23:21:48 +0200

    Završen zadatak

--- src/ba/unsa/etf/rpr/Korpa.java
+++ src/ba/unsa/etf/rpr/Korpa.java
@@ -1,4 +1,43 @@
 package ba.unsa.etf.rpr;
 
+import java.util.ArrayList;
+
 public class Korpa {
+    ArrayList<Artikl> listaArtikalaUKorpi = new ArrayList<Artikl>(50);
+
+    public boolean dodajArtikl(Artikl a) {
+        if (listaArtikalaUKorpi.size()<50) {
+            listaArtikalaUKorpi.add(a);
+            return true;
+        }
+        return false;
+    }
+
+    public Artikl[] getArtikli() {
+        Artikl[] artikli = new Artikl[50];
+        int i = 0;
+        for(Artikl a : listaArtikalaUKorpi) {
+            artikli[i] = a;
+            i = i + 1;
+        }
+        return artikli;
+    }
+
+    public Artikl izbaciArtiklSaKodom(String kod) {
+        for (Artikl a : listaArtikalaUKorpi) {
+            if (a!=null && a.getKod().equals(kod)) {
+                listaArtikalaUKorpi.remove(a);
+                return a;
+            }
+        }
+        return null;
+    }
+
+    public int dajUkupnuCijenuArtikala() {
+        int ukupnaCijenaArtikala = 0;
+        for (Artikl a : listaArtikalaUKorpi) {
+            ukupnaCijenaArtikala = ukupnaCijenaArtikala + a.getCijena();
+        }
+        return ukupnaCijenaArtikala;
+    }
 }
--- src/ba/unsa/etf/rpr/Supermarket.java
+++ src/ba/unsa/etf/rpr/Supermarket.java
@@ -1,4 +1,33 @@
 package ba.unsa.etf.rpr;
 
+import java.util.ArrayList;
+
 public class Supermarket {
+    ArrayList<Artikl> listaArtikalaUSupermarketu = new ArrayList<Artikl>(1000);
+
+    public Artikl[] getArtikli() {
+        Artikl[] artikli = new Artikl[50];
+        int i = 0;
+        for(Artikl a : listaArtikalaUSupermarketu) {
+            artikli[i] = a;
+            i = i + 1;
+        }
+        return artikli;
+    }
+
+    public Artikl izbaciArtiklSaKodom(String kod) {
+        for (Artikl a : listaArtikalaUSupermarketu) {
+            if (a!=null && a.getKod().equals(kod)) {
+                listaArtikalaUSupermarketu.remove(a);
+                return a;
+            }
+        }
+        return null;
+    }
+
+    public void dodajArtikl(Artikl a) {
+        if (listaArtikalaUSupermarketu.size()<1000) {
+            listaArtikalaUSupermarketu.add(a);
+        }
+    }
 }
